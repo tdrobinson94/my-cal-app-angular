1f8b480118a4871454c153c1a354c798f1562877
Terry Robinson <tdrobinson94@comcast.net>
2020-04-20 14:42:05 -0400

    calendar updates

--- src/app/test-calendar/test-calendar.component.ts
+++ src/app/test-calendar/test-calendar.component.ts
@@ -88,7 +88,7 @@ export class TestCalendarComponent implements OnInit, AfterViewInit {
 
     if (navigator.userAgent.indexOf('Mac') !== -1) {
       console.log('Mac');
-      $('.calendar-nav-bar').addClass('mac');
+      $('.calendar-navbar').addClass('mac');
     } else if (navigator.userAgent.indexOf('Win') !== -1) {
       console.log('Windows');
     }
