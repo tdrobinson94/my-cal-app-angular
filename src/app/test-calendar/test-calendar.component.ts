08333c5d329826f80482ffc31be9e5f557786409
Terry Robinson <tdrobinson94@comcast.net>
2020-04-20 14:32:38 -0400

    calendar updates

--- src/app/test-calendar/test-calendar.component.ts
+++ src/app/test-calendar/test-calendar.component.ts
@@ -86,10 +86,11 @@ export class TestCalendarComponent implements OnInit, AfterViewInit {
       this.yearSelectorOptions.push(yearStart + i);
     }
 
-    if (navigator.userAgent.indexOf('Win') !== -1) {
+    if (navigator.userAgent.indexOf('Mac') !== -1) {
+      console.log('Mac');
+      $('.calendar-nav-bar').addClass('mac');
+    } else if (navigator.userAgent.indexOf('Win') !== -1) {
       console.log('Windows');
-    } else {
-      $('.calendar-container').addClass('mac');
     }
   }
 
