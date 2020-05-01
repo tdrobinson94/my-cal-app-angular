8fc85cc701879e350a9940487bea1e51d9352d89
Terry Robinson <tdrobinson94@comcast.net>
2020-05-01 13:05:47 -0400

    added swipe left and right for mobile

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -127,6 +127,11 @@ export class CalendarComponent implements OnInit, AfterViewInit, OnDestroy {
     } else if (navigator.userAgent.indexOf('Win') !== -1) {
       // console.log('Windows');
     }
+
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)) {
+      $('.prev, .next').hide();
+    }
   }
 
   ngAfterViewInit() {
