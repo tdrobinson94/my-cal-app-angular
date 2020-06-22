ccbfd5e53fde2f339eb394d2735e576066096a1d
Terry Robinson <tdrobinson94@comcast.net>
2020-06-22 13:13:31 -0400

    updated styling on calendar for larger displays and laptops

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -590,6 +590,12 @@ export class CalendarComponent implements OnInit, AfterViewInit, OnDestroy {
         $('.double-click').find('.main-info-section').addClass('animate-events-two');
       }, 400);
       console.log($('.double-click .main-info-section .visible').length);
+      
+      if ($('.double-click .main-info-section').height() <= $('.double-click .visible-parent').last().position().top) {
+        $('.double-click .main-info-section').addClass('normal-scrolling');
+      } else {
+        $('.double-click .main-info-section').removeClass('normal-scrolling');
+      }
 
       setTimeout(() => {
         $('.double-click .num-date').addClass('auto-hide');
@@ -626,6 +632,7 @@ export class CalendarComponent implements OnInit, AfterViewInit, OnDestroy {
             if (day.find('.date-value').html() === eventlist[i].eventstart_date ) {
               setTimeout(() => {
                 day.find('.event[startDate="' + day.find('.date-value').html() + '"]').addClass('visible');
+                day.find('.event[startDate="' + day.find('.date-value').html() + '"]').parent().addClass('visible-parent');
               }, 100);
             }
 
